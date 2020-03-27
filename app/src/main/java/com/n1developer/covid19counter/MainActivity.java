abfd09a9796a6da4b9a61ef655bc9cb4b5e56c67
Usama Tariq <usamat37@gmail.com>
2020-03-27 10:09:47 -0700

    My new Comit

--- app/src/main/java/com/n1developer/covid19counter/MainActivity.java
+++ app/src/main/java/com/n1developer/covid19counter/MainActivity.java
@@ -112,29 +112,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void loadAdd()
     {
-//        AdLoader adLoader = new AdLoader.Builder(this, "ca-app-pub-3940256099942544/2247696110")
-//                .forUnifiedNativeAd(new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
-//                    @Override
-//                    public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
-//                        // Show the ad.
-//                    }
-//                })
-//                .withAdListener(new AdListener() {
-//                    @Override
-//                    public void onAdFailedToLoad(int errorCode) {
-//                        // Handle the failure by logging, altering the UI, and so on.
-//                    }
-//                })
-//                .withNativeAdOptions(new NativeAdOptions.Builder()
-//                        // Methods in the NativeAdOptions.Builder class can be
-//                        // used here to specify individual options settings.
-//                        .build())
-//                .build();
-//
-//        adLoader.loadAd(new AdRequest.Builder().build());
-
-        AdView a = findViewById(R.id.adView2);
-        a.loadAd(new AdRequest.Builder().build());
     }
 
     @Override
